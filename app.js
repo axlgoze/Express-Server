52fa4e9fa7c28f0826e98d898676335eea0a0b81
Axel Reyes <rbaxel.f@gmail.com>
2022-04-30 19:18:34 -0500

    add: initial path (1)

--- app.js
+++ app.js
@@ -7,3 +7,9 @@ const app = require()
 //Puerto en vamos a ver nuestra app: localhost: 3000
 const port = 3000
 
+//path inicial, responderá a la url localhost: 3000
+app.get('/', (req, res) =>{
+	res.send('Hello World')
+})
+
+
