618c615cd806df20ccf1901c7e9ff1d15cd4440b
Axel Reyes <rbaxel.f@gmail.com>
2022-04-30 19:12:08 -0500

    add: express object,app and port (0)

---
+++ app.js
@@ -0,0 +1,9 @@
+//Creando primera app de express
+
+//Usando objeto express
+const express = require('express')
+//App de express
+const app = require()
+//Puerto en vamos a ver nuestra app: localhost: 3000
+const port = 3000
+
